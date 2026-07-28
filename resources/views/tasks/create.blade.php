aa5884eacfb8cfd8b398489c099f848c00f86b4c
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2026-07-29 07:11:37 +0900

    改善：バリデーションエラーと入力値の保持を表示

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,16 +1,24 @@
 <h1>新規タスク</h1>
 
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
 <form action="{{ route('tasks.store') }}" method="post">
     @csrf
     
     <div>
         <label>タイトル</label>
-        <input type="text" name="title">
+        <input type="text" name="title" value="{{ old('title') }}">
     </div>
 
     <div>
         <label>本文</label>
-        <textarea name="body"></textarea>
+        <textarea name="body">{{ old('body') }}</textarea>
     </div>
 
     <button type="submit">保存</button>
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,15 +1,23 @@
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
 <form action="{{ route('tasks.update', $task) }}" method="post">
     @csrf
     @method('PUT')
 
     <div>
         <label>タイトル</label>
-        <input type="text" name="title" value="{{ $task->title }}">
+        <input type="text" name="title" value="{{ old('title', $task->title) }}">
     </div>
 
     <div>
         <label>本文</label>
-        <textarea name="body">{{ $task->body }}</textarea>
+        <textarea name="body">{{ old('body', $task->body) }}</textarea>
     </div>
 
     <button type="submit">保存</button>
